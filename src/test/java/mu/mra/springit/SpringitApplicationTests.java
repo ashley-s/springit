04186b7f81593020d1d2ab826d28685aebdf7451
Ashley Shookhye <ashley.shookhye@gmail.com>
2020-12-07 22:29:09 +0400

    Initial Commit for our Spring Boot Reddit Clone!x

---
+++ src/test/java/mu/mra/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.mra.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
